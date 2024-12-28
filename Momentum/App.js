580a248461c11cf036e682a871e28ae41ab3bcd7
michele <mbarrant@iu.edu>
2024-12-28 12:29:05 -0500

    Project strcuture

--- Momentum/App.js
+++ Momentum/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import HistoryScreen from './src/screens/HistoryScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Tab.Navigator>
+
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="History" component={HistoryScreen}/>
+        <Tab.Screen name="Settings" component={SettingsScreen}/>
+  
+      </Tab.Navigator>
+
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
